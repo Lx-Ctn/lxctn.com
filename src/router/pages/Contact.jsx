2996680294a1e9cc1bb1c83e882a26dbb1f368a0
Alexandre Cottin (Lx-Ctn) <design@lxctn.com>
2023-07-27 17:51:31 +0200

    fix: adapte page transition to first load

--- src/router/pages/Contact.jsx
+++ src/router/pages/Contact.jsx
@@ -5,17 +5,19 @@ import { useSelector } from "react-redux";
 
 export const Contact = () => {
 	const reducedMotion = useSelector(state => state.app.prefersReducedMotion);
+	const isIntro = useSelector(state => state.header.isIntro);
+
 	return (
-		<motion.div className={css._} {...(!reducedMotion && animation)}>
+		<motion.div className={css._} {...(!reducedMotion && animation(isIntro))}>
 			<div className={css.bubble} />
 			<ContactCard />
 		</motion.div>
 	);
 };
 
-const animation = {
+const animation = isIntro => ({
 	initial: { scale: 0.6, opacity: 0 },
-	animate: { scale: 1, opacity: 1, transition: { duration: 0.2 } },
+	animate: { scale: 1, opacity: 1, transition: { duration: 0.2, delay: isIntro ? 0.7 : 0 } },
 	exit: { scale: 0.4, opacity: 0 },
 	transition: { duration: 0.3 },
-};
+});
--- src/router/pages/Home.jsx
+++ src/router/pages/Home.jsx
@@ -1,62 +1,46 @@
 import css from "./Home.module.scss";
 import { useOutlet, useLocation } from "react-router-dom";
 import { Avatar, AnimatedTitle } from "../../components";
-import { DelayRender } from "../../components/utils";
 import { motion, AnimatePresence } from "framer-motion";
 import { useSelector } from "react-redux";
 
 export const Home = () => {
 	const location = useLocation();
 	const reducedMotion = useSelector(state => state.app.prefersReducedMotion);
-	const isIntro = useSelector(state => state.header.isIntro);
-	return reducedMotion ? (
-		<div className={`${css._} ${location.pathname === "/contact" ? css.contact : null}`}>
-			<div className={css.avatar_container}>
-				<Avatar />
-			</div>
-			<Outlet />
-		</div>
-	) : (
-		<motion.div
-			key={"home"}
+
+	return (
+		<div
 			className={`${css._} ${location.pathname === "/contact" ? css.contact : null}`}
-			{...homePageAnimation}
+			{...(!reducedMotion && homePageAnimation)}
 		>
 			<div className={css.avatar_container}>
 				<Avatar />
 			</div>
-
-			{isIntro ? (
-				<DelayRender delay={700}>
-					{/* Outlet to render HomeElement or Contact as sub-route :
+			{/* Outlet to render HomeElement or Contact as sub-route :
 				-> to keep Avatar on screen and alow a smooth transition */}
-					<Outlet />
-				</DelayRender>
-			) : (
-				<Outlet />
-			)}
-		</motion.div>
-	);
-};
-
-export const HomeElement = () => {
-	const reducedMotion = useSelector(state => state.app.prefersReducedMotion);
-	return reducedMotion ? (
-		<h1>Lx Design</h1>
-	) : (
-		<motion.div {...homeElementAnimation}>
-			<AnimatedTitle />
-		</motion.div>
+			<Outlet />
+		</div>
 	);
 };
 
 const Outlet = () => {
 	const routeElement = useOutlet();
-
 	return (
 		<AnimatePresence mode="wait">
 			<div key={window.location.pathname}>{routeElement}</div>
 		</AnimatePresence>
+	);
+};
+
+export const HomeElement = () => {
+	const reducedMotion = useSelector(state => state.app.prefersReducedMotion);
+	const isIntro = useSelector(state => state.header.isIntro);
+	return reducedMotion ? (
+		<h1>Lx Design</h1>
+	) : (
+		<motion.div {...homeElementAnimation(isIntro)}>
+			<AnimatedTitle />
+		</motion.div>
 	);
 };
 
@@ -66,9 +50,9 @@ const homePageAnimation = {
 	exit: { scale: 0.4, opacity: 0, transition: { duration: 0.2 } },
 	transition: { duration: 0.4 },
 };
-const homeElementAnimation = {
+const homeElementAnimation = isIntro => ({
 	initial: { scale: 0.4, opacity: 0 },
 	animate: { scale: 1, opacity: 1 },
 	exit: { scale: 0.4, opacity: 0, transition: { duration: 0.2 } },
-	transition: { duration: 0.4 },
-};
+	transition: { duration: 0.4, delay: isIntro ? 0.7 : 0 },
+});
